a30f826ac60e85bf7e24544627fa385b1ca65b9e
tang <693795240@qq.com>
2021-03-01 10:56:34 +0800

    页面优化

--- src/views/fund/fund-basic.vue
+++ src/views/fund/fund-basic.vue
@@ -1,39 +1,183 @@
 <template>
   <div class="chart-container">
-    <el-button type="primary" @click="getfundBasic">查询</el-button>
+    <el-row>
+      <el-col
+        :span="12"
+        :style="'display:flex;flex-direction:row;justify-content:flex-start'"
+      >
+        <el-input
+          v-model="period"
+          placeholder="计算数量"
+          :style="'width:250px'"
+        >
+          <template
+            slot="prepend"
+          >计算天数</template>
+          <el-button
+            slot="append"
+            icon="el-icon-search"
+            @click="getfundBasic"
+          />
+        </el-input>
+      </el-col>
+      <el-col
+        :span="12"
+        :style="'display:flex;flex-direction:row;justify-content:flex-end'"
+      >
+        <el-button type="primary" @click="forgetfundPredict">估值</el-button>
+        <el-button type="primary" @click="stopsend">停止估值</el-button></el-col>
+    </el-row>
+    <!-- <el-button type="primary" @click="getfundBasic">查询</el-button>
     <el-button type="primary" @click="forgetfundPredict">估值</el-button>
     <el-button type="primary" @click="stopsend">停止估值</el-button>
-    <el-input v-model="period" placeholder="计算数量" />
+    <el-input v-model="period" placeholder="计算数量" /> -->
 
     <el-table :data="fundNavDetail" border stripe fit :cell-style="cellStyle">
-      <el-table-column  label="基金代码" width="100">
+      <el-table-column label="基金代码" width="100">
+        <!-- <template slot-scope="scope">
+          <div>{{ scope.row.ts_code }}</div>
+          <div>{{ scope.row.name }}</div>
+        </template> -->
         <template slot-scope="scope">
-          <div>{{scope.row.ts_code}}</div>
-          <div>{{scope.row.name}}</div>
+          <el-popover trigger="hover" placement="top">
+            <p>投资类型: {{ scope.row.fund_type }}</p>
+            <p>成立日期: {{ scope.row.found_date }}</p>
+            <p>管理人: {{ scope.row.management }}</p>
+            <p>业绩比较基准: {{ scope.row.benchmark }}</p>
+            <div slot="reference" class="name-wrapper">
+              <div>{{ scope.row.ts_code }}</div>
+              <div>{{ scope.row.name }}</div>
+              <el-tag
+                v-if="
+                  scope.row.is_increase_count <= -3 &&
+                    scope.row.change_count_percent_rank <= 0.2 &&
+                    scope.row.calmar_ratio_rank >= 0.8
+                "
+                slot="reference"
+                type="el-icon-circle-plus"
+              >{{ `加仓` }}</el-tag>
+              <el-tag
+                v-else-if="
+                  scope.row.is_increase_count > 3 &&
+                    scope.row.is_increase_count >= 0.99 &&
+                    scope.row.change_count_percent_rank <= 0.2
+                "
+                slot="reference"
+                type="danger"
+              >{{ `减仓` }}</el-tag>
+            </div>
+          </el-popover>
         </template>
       </el-table-column>
       <!-- <el-table-column prop="name" label="简称" /> -->
-      <el-table-column prop="pct_rank" label="近180日价格排名" sortable />
-      <el-table-column prop="diff_pct_rank" label="变动排名" sortable >
+      <el-table-column
+        prop="pct_rank"
+        label="近180日价格排名"
+        sortable
+        :formatter="pctRankFormatter"
+      />
+      <el-table-column prop="diff_pct_rank" label="变动排名" sortable>
         <template slot-scope="scope">
-          <el-tag type="danger" slot="reference" v-if="scope.row.diff_pct_rank.indexOf('-')!=0">{{ scope.row.diff_pct_rank }}</el-tag>
-          <el-tag type="success" slot="reference" v-else-if="scope.row.diff_pct_rank.indexOf('-')==0">{{ scope.row.diff_pct_rank }}</el-tag>
+          <el-tag
+            v-if="scope.row.diff_pct_rank <= 0"
+            slot="reference"
+            type="success"
+          >{{ `${(scope.row.diff_pct_rank * 100).toFixed(2)}%` }}</el-tag>
+          <el-tag
+            v-else-if="scope.row.diff_pct_rank > 0"
+            slot="reference"
+            type="danger"
+          >{{ `${(scope.row.diff_pct_rank * 100).toFixed(2)}%` }}</el-tag>
         </template>
       </el-table-column>
-
 
       <el-table-column prop="std" label="标准差" sortable />
       <el-table-column prop="sortino_ratio" label="索提诺比率" sortable />
       <!-- <el-table-column prop="sharp" label="夏普比率" sortable /> -->
-      <el-table-column prop="daily_value_at_risk" label="每日风险价值" sortable />
+      <el-table-column
+        prop="daily_value_at_risk"
+        label="每日风险价值"
+        sortable
+      />
       <!-- <el-table-column prop="max_drawdown" label="最大回撤" sortable /> -->
-      <el-table-column prop="is_increase_count" label="最新涨跌次数" sortable />
+      <el-table-column prop="is_increase_count" label="连续涨跌次数" sortable />
+      <el-table-column
+        prop="change_count_percent"
+        label="当前涨跌次数出现概率"
+        sortable
+        :formatter="changeCountPercent"
+      />
 
       <el-table-column prop="calmar_ratio" label="calmar_ratio" sortable />
       <el-table-column prop="tail_ratio" label="tail_ratio" sortable />
-      <el-table-column prop="adj_nav_date_near_today" label="上次最近日期" sortable />
-      <el-table-column prop="adj_nav_near_today" label="上次最近净值" sortable />
+      <el-table-column
+        prop="adj_nav_date_near_today"
+        label="上次最近日期"
+        sortable
+      />
+      <el-table-column prop="downside_risk" label="下降风险" sortable />
+      <el-table-column prop="omega_ratio" label="omiga比率" sortable />
 
+      <!-- <el-table-column
+        prop="adj_nav_near_today"
+        label="上次最近净值"
+        sortable
+      /> -->
+      <el-table-column prop="mean_5" label="5日均价偏离（警）" sortable>
+        <template slot-scope="scope">
+          <el-tag v-if="scope.row.mean_5 < 0" slot="reference" type="success">{{
+            `${(scope.row.mean_5 * 100).toFixed(2)}%`
+          }}</el-tag>
+          <el-tag
+            v-else-if="scope.row.mean_5 == 0"
+            slot="reference"
+            type="info"
+          >{{ `${(scope.row.mean_10 * 100).toFixed(2)}%` }}</el-tag>
+          <el-tag
+            v-else-if="scope.row.mean_5 > 0"
+            slot="reference"
+            type="danger"
+          >{{ `${(scope.row.mean_5 * 100).toFixed(2)}%` }}</el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column prop="mean_10" label="10日均价偏离（减）" sortable>
+        <template slot-scope="scope">
+          <el-tag
+            v-if="scope.row.mean_10 < 0"
+            slot="reference"
+            type="success"
+          >{{ `${(scope.row.mean_10 * 100).toFixed(2)}%` }}</el-tag>
+          <el-tag
+            v-else-if="scope.row.mean_10 == 0"
+            slot="reference"
+            type="info"
+          >{{ `${(scope.row.mean_10 * 100).toFixed(2)}%` }}</el-tag>
+          <el-tag
+            v-else-if="scope.row.mean_10 > 0"
+            slot="reference"
+            type="danger"
+          >{{ `${(scope.row.mean_10 * 100).toFixed(2)}%` }}</el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column prop="mean_20" label="20日均价偏离（清）" sortable>
+        <template slot-scope="scope">
+          <el-tag
+            v-if="scope.row.mean_20 < 0"
+            slot="reference"
+            type="success"
+          >{{ `${(scope.row.mean_20 * 100).toFixed(2)}%` }}</el-tag>
+          <el-tag
+            v-else-if="scope.row.mean_20 == 0"
+            slot="reference"
+            type="info"
+          >{{ `${(scope.row.mean_20 * 100).toFixed(2)}%` }}</el-tag>
+          <el-tag
+            v-else-if="scope.row.mean_20 > 0"
+            slot="reference"
+            type="danger"
+          >{{ `${(scope.row.mean_20 * 100).toFixed(2)}%` }}</el-tag>
+        </template>
+      </el-table-column>
       <el-table-column label="3年评级" width="150" sortable>
         <template slot-scope="scope">
           <el-rate
@@ -69,30 +213,6 @@
         </template>
       </el-table-column>
       <!-- <el-table-column prop="risk" label="索提诺/夏普比率" sortable /> -->
-      <!-- <el-table-column prop="omega_ratio" label="omiga比率" sortable /> -->
-      <el-table-column prop="downside_risk" label="下降风险" sortable />
-
-      <el-table-column prop="mean_5" label="5日均价偏离（警）" sortable >
-        <template slot-scope="scope">
-          <el-tag type="success" slot="reference" v-if="scope.row.mean_5<0">{{ scope.row.mean_5 }}</el-tag>
-          <el-tag type="info" slot="reference" v-else-if="scope.row.mean_5==0">{{ scope.row.mean_5 }}</el-tag>
-          <el-tag type="danger" slot="reference" v-else-if="scope.row.mean_5>0">{{ scope.row.mean_5 }}</el-tag>
-        </template>
-      </el-table-column>
-      <el-table-column prop="mean_10" label="10日均价偏离（减）" sortable >
-        <template slot-scope="scope">
-          <el-tag type="success" slot="reference" v-if="scope.row.mean_10<0">{{ scope.row.mean_10 }}</el-tag>
-          <el-tag type="info" slot="reference" v-else-if="scope.row.mean_10==0">{{ scope.row.mean_10 }}</el-tag>
-          <el-tag type="danger" slot="reference" v-else-if="scope.row.mean_10>0">{{ scope.row.mean_10 }}</el-tag>
-        </template>
-      </el-table-column>
-      <el-table-column prop="mean_20" label="20日均价偏离（清）" sortable >
-           <template slot-scope="scope">
-          <el-tag type="success" slot="reference" v-if="scope.row.mean_20<0">{{ scope.row.mean_20 }}</el-tag>
-          <el-tag type="info" slot="reference" v-else-if="scope.row.mean_20==0">{{ scope.row.mean_20 }}</el-tag>
-          <el-tag type="danger" slot="reference" v-else-if="scope.row.mean_20>0">{{ scope.row.mean_20 }}</el-tag>
-        </template>
-      </el-table-column>
 
       <el-table-column label="今日净值" prop="dwjz" sortable />
       <el-table-column label="估算净值" prop="gsz" sortable />
@@ -101,15 +221,28 @@
 
       <!-- <el-table-column prop="max_income_pct" label="最大收益率" sortable /> -->
       <!-- <el-table-column label="潜力" prop="potential" sortable /> -->
+      <!-- <el-table-column label="其他信息">
+        <template slot-scope="scope">
+          <el-popover trigger="hover" placement="top">
+            <p>投资类型: {{ scope.row.fund_type }}</p>
+            <p>成立日期: {{ scope.row.found_date }}</p>
+            <p>管理人: {{ scope.row.management }}</p>
+            <p>业绩比较基准: {{ scope.row.benchmark }}</p>
 
-      <el-table-column prop="fund_type" label="投资类型" />
+            <div slot="reference" class="name-wrapper">
+              <el-tag size="medium">其他信息</el-tag>
+            </div>
+          </el-popover>
+        </template>
+      </el-table-column> -->
+      <!-- <el-table-column prop="fund_type" label="投资类型" /> -->
       <!-- <el-table-column prop="management" label="管理人" sortable /> -->
-      <el-table-column prop="found_date" label="成立日期" />
+      <!-- <el-table-column prop="found_date" label="成立日期" /> -->
       <!-- <el-table-column prop="issue_amount" label="发行份额(亿)" /> -->
       <!-- <el-table-column prop="net_asset" label="资产净值" /> -->
       <!-- <el-table-column prop="p_value" label="面值" /> -->
       <!-- <el-table-column prop="exp_return" label="预期收益率" /> -->
-      <el-table-column prop="benchmark" label="业绩比较基准" />
+      <!-- <el-table-column prop="benchmark" label="业绩比较基准" /> -->
       <!-- <el-table-column prop="invest_type" label="投资风格" /> -->
       <!-- <el-table-column prop="type" label="基金类型" /> -->
       <!-- <el-table-column prop="purc_startdate" label="日常申购起始日" /> -->
@@ -118,7 +251,7 @@
           <router-link
             :to="{
               name: 'FundsNav',
-              query: { fundCode: scope.row.ts_code.slice(0, 6) }
+              query: { fundCode: scope.row.ts_code }
             }"
           >
             <el-button type="primary">详情</el-button>
@@ -235,6 +368,12 @@ export default {
       // setInterval(loopTest, 2000);
       loopTest()
       alert('实时价格同步开始')
+    },
+    pctRankFormatter(row, column) {
+      return `${(row.pct_rank * 100).toFixed(2)}%`
+    },
+    changeCountPercent(row, column) {
+      return `${(row.change_count_percent * 100).toFixed(2)}%`
     }
   }
 }
